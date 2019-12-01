3bc5d33986cad592cd24fe16af8d31c1a9e6141f
Peter Doornbosch <peter.doornbosch@luminis.eu>
2019-12-01 22:14:29 +0100

    rewrite sample to generic http3 file downloader

--- src/main/java/net/luminis/http3/sample/AsyncHttp3.java
+++ src/main/java/net/luminis/http3/sample/AsyncHttp3.java
@@ -2,56 +2,59 @@ package net.luminis.http3.sample;
 
 import net.luminis.http3.Http3Client;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 
 public class AsyncHttp3 {
 
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
-            System.out.println("Excpected arguments: <server:port> <path> [<path>....]");
+            System.out.println("Excpected arguments: <download-dir> <url1> [<url2>, ....]");
             return;
         }
 
-        String serverAddress = args[0];
+        File downloadDir = new File(args[0]);
+        if (downloadDir.exists() && !downloadDir.isDirectory()) {
+            System.out.println("'" + downloadDir + "' exists, but is not a directory");
+            return;
+        }
+        if (! downloadDir.exists()) {
+            downloadDir.mkdir();
+        }
+
+        URI[] downloadUrls = new URI[args.length-1];
+        for (int i = 0; i < args.length-1; i++) {
+            downloadUrls[i] = new URI(args[i+1]);
+        }
 
         HttpClient client = Http3Client.newHttpClient();
-        CompletableFuture<HttpResponse<String>>[] results = new CompletableFuture[args.length - 1];
-        for (int i = 0; i < args.length-1; i++) {
-            String path = args[i+1];
-            URI requestUri = URI.create("https://" + serverAddress + "/" + path);
-            HttpRequest request = HttpRequest.newBuilder().uri(requestUri).build();
-            HttpResponse<String> httpResponse = null;
+        CompletableFuture<HttpResponse<Path>>[] results = new CompletableFuture[downloadUrls.length];
+        for (int i = 0; i < downloadUrls.length; i++) {
+            HttpRequest request = HttpRequest.newBuilder().uri(downloadUrls[i]).build();
+            String outputFile = new File(downloadDir, downloadUrls[i].getPath()).getAbsolutePath();
+            HttpResponse.BodyHandler<Path> responseBodyHandler = HttpResponse.BodyHandlers.ofFile(Paths.get(outputFile));
 
-            System.out.println("Starting asynchronous request for " + requestUri);
-            results[i] = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-            results[i].thenApply(response -> { System.out.println("Already done: " + response); return true; });
+            System.out.println("Starting asynchronous request for " + downloadUrls[i]);
+            results[i] = client.sendAsync(request, responseBodyHandler);
+            results[i].thenApply(response -> {
+                System.out.println("Done: " + response + ", response body written to " + outputFile);
+                return true;
+            });
         }
 
         // Wait for all requests to finish.
-        for (int i = 0; i < args.length-1; i++) {
+        for (int i = 0; i < downloadUrls.length; i++) {
             results[i].get();
-        }
-
-        // Print results
-        for (int i = 0; i < args.length-1; i++) {
-            HttpResponse<String> httpResponse = results[i].get();
-            System.out.println("Got HTTP response " + httpResponse);
-            System.out.println("-   HTTP headers: " + httpResponse.headers());
-            System.out.println("-   HTTP body (" + httpResponse.body().length() + " bytes):");
-            String outputFile = "http-async-response" + i + ".txt";
-            try {
-                Files.write(Paths.get(outputFile), httpResponse.body().getBytes());
-                System.out.println("Response written to file: " + outputFile);
-            } catch (IOException e) {
-                System.out.println("Writing result to file failed: "+ e);
-            }
         }
 
         System.out.println("Terminating.");
