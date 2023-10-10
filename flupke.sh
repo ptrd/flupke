111bf0425e4ef18f794ac8e02a32117d1207c6b2
Peter Doornbosch <peter.doornbosch@luminis.eu>
2023-10-10 18:35:39 +0200

    added tasks to publish flupke to maven central

--- flupke.sh
+++ flupke.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [ ! -f build/libs/flupke-uber.jar ]; then
+flupkejar=`ls build/libs/flupke-uber*.jar | grep -v javadoc | grep -v sources 2> /dev/null`
+if [ ! -f "$flupkejar" ]; then
     echo "Cannot find flupke-uber.jar. Did you forgot to build it? ('gradle uberJar')"
     exit
 fi
-
-java -cp build/libs/flupke-uber.jar net.luminis.http3.sample.Sample $*
+java -cp $flupkejar net.luminis.http3.sample.Sample $*
